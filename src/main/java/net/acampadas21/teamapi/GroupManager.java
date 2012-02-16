b62a42485daf3bbb8fd416ebdb782bd056acd535
jertocvil <jertocvil@gmail.com>
2012-02-17 00:07:27 +0100

    Adding missing class

---
+++ src/main/java/net/acampadas21/teamapi/GroupManager.java
@@ -0,0 +1,19 @@
+package net.acampadas21.teamapi;
+
+import lib.PatPeter.SQLibrary.SQLite;
+
+public class GroupManager {
+
+	private SQLite db;
+
+	public GroupManager() {
+		try {
+			db = new SQLite(TeamAPI.logger, "TeamAPI", "teamapi",
+					"./plugins/TeamAPI");
+			db.open();
+		} catch (Exception e) {
+			throw new Error("Can't connect to the database");
+		}
+	}
+
+}
