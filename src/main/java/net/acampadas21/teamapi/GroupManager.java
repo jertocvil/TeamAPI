3c5c5dc2e2e5606761fa1f9ed99e66290d364b0a
jertocvil <jertocvil@gmail.com>
2012-02-17 10:13:16 +0100

    Implementation of GroupManager started.

--- src/main/java/net/acampadas21/teamapi/GroupManager.java
+++ src/main/java/net/acampadas21/teamapi/GroupManager.java
@@ -1,6 +1,9 @@
 package net.acampadas21.teamapi;
 
 import lib.PatPeter.SQLibrary.SQLite;
+import net.acampadas21.teamapi.groups.Team;
+
+import org.bukkit.entity.Player;
 
 public class GroupManager {
 
@@ -15,11 +18,26 @@ public class GroupManager {
 			throw new Error("Can't connect to the database");
 		}
 	}
-	
-	public boolean isTeam(String name){
+
+	public boolean isTeam(String name) {
 		return db.checkTable(name);
 	}
-	
-	public 
 
+	public void newTeam(String name) {
+		if (!isTeam(name)) {
+			db.createTable("CREATE TABLE " + name + "(player VARCHAR(20), leader TINYINT);");
+		}
+	}
+	
+	public void joinTeam(Player p, Team t){
+			db.query("INSERT INTO " + t.getName() + " VALUES("+ t.getName() +", 0);");
+	}
+	
+	public void deleteTeam(Team t){
+		if(isTeam(t.getName())){
+			db.query("DELETE TABLE " + t.getName() + ";");
+		}
+	
+	}
+	
 }
